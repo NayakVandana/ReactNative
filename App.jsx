6ccff940056faf424f50175905b0fc3cb62638aa
Vandana Nayak <vandana.nayak@binarysphere.com>
2024-07-31 17:29:01 +0530

    Style in React Native

--- App.jsx
+++ App.jsx
@@ -1,16 +1,36 @@
 import React, { useState } from 'react';
 
-import {Button, Text, View} from 'react-native';
-import User from './components/User'
+import {Button, Text, View,StyleSheet} from 'react-native';
+import Exstyles from './style'
 const App = () => {
-  const [name,setName] = useState("Test");
+  
   return (
     <View>
-      <Text style={{fontSize: 30}}>Text Components</Text>
-      <Button title='Update Props' onPress={()=>setName('Test1')}></Button>
-      <User name={name} age={29}/>
+      <Text style={{fontSize: 30,color:'red',backgroundColor:'green'}}>Text Components</Text>
+      <Text style={styles.textBox}>Text Components</Text>
+      <Text style={styles.textBox}>Text Components</Text>
+      <Text style={styles.textBox}>Text Components</Text>   
+      <Text style={Exstyles.textBox}>Text Components</Text>      
+      <Text style={[styles.textBox,Exstyles.textBox,{marginTop:20}]}>Text Components</Text>     
     </View>
   );
 };
+
+const styles=StyleSheet.create({
+  textBox:{
+    color:'#fff',
+    fontSize:25,
+    backgroundColor:'blue',
+    marginBottom:10,
+    padding:10,
+    borderRadius:10,
+    height:100,
+    textAlignVertical:'center',
+    textAlign:'center',
+    borderColor:'red',
+    borderWidth:2,
+
+  }
+})
 
 export default App;
