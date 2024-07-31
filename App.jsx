62ae4835f088d83881a1b14673f628c58ea18586
Vandana Nayak <vandana.nayak@binarysphere.com>
2024-07-31 12:58:33 +0530

    create components

--- App.jsx
+++ App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
 import {Button, Text, View} from 'react-native';
-
+import CompanyData from './components/CompanyData'
 const App = () => {
   return (
     <View>
-      <Text style={{fontSize: 30}}>Text Component</Text>
+      <Text style={{fontSize: 30}}>Text Components</Text>
       <Button title='Press Here'></Button>
-      <Text style={{fontSize: 20}}>Text Component</Text>
+      <Text style={{fontSize: 20}}>Text Components</Text>
       <Button title='Press Me To'></Button>
+      <CompanyData/>
     </View>
   );
 };
